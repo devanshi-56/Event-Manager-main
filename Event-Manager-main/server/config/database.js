96c5a70a7ad3d9bf5b9ef64bc429c866e6fea5a3
devanshi-56 <devanshi2001.gupta@gmail.com>
2022-03-04 20:16:50 +0530

    sprint-1 code

---
+++ Event-Manager-main/server/config/database.js
@@ -0,0 +1,6 @@
+require('dotenv').config({path: ".././.env"});
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    return mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+};
